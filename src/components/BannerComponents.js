d4fd0d90bafb4f36e72b49928444118e58584fe6
HuyLuong <huyluong131001@gmail.com>
2023-08-22 22:48:19 +0700

    fix class -> className

--- src/components/BannerComponents.js
+++ src/components/BannerComponents.js
@@ -4,13 +4,13 @@ export default class BannerComponents extends Component {
     render() {
         return (
             <div className="container mt-3 bg-body-secondary p-5">
-                <p class="fs-1">A Warm Welcome</p>
+                <p className="fs-1">A Warm Welcome</p>
                 <p>
                     Bootstrap utility classes are used to create this jumbotron
                     since the old component has been removed from the framework.
                     Why create custom CSS when you can use utilities?
                 </p>
-                <button type="button" class="btn btn-primary">
+                <button type="button" className="btn btn-primary">
                     Call to action!
                 </button>
             </div>
